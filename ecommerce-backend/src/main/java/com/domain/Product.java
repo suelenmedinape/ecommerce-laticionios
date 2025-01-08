34b2937091a59e8fdd710ff4a281c9409f55d582
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-07 22:52:32 -0300

    feat: adicionar classe Product para chave composta com JPA

--- ecommerce-backend/src/main/java/com/domain/Product.java
+++ ecommerce-backend/src/main/java/com/domain/Product.java
@@ -1,5 +1,64 @@
 package com.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String productName;
+	private String description;
+	private Double price;
+	private Integer quantity;
+	
+	public Product() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
 }
