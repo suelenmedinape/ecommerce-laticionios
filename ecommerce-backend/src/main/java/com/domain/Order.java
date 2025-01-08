1b48f11aecded95733795cae299d2f39b36d9092
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-07 22:41:32 -0300

    feat: adicionar classe Order com mapeamento JPA

--- ecommerce-backend/src/main/java/com/domain/Order.java
+++ ecommerce-backend/src/main/java/com/domain/Order.java
@@ -1,5 +1,74 @@
 package com.domain;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date date;
+	private Double totalValue;
+	
+	@ManyToOne
+	private Client client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private List<OrderItem> orderItems = new ArrayList<>();
+	
+	public Order() {
+		super();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(Double totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItem> orderItens) {
+		this.orderItems = orderItens;
+	}
 }
