ef71f6a6ecfcda84bdc6865c32cf0b90b916cc37
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-07 22:39:19 -0300

    feat: adicionar classe Producer com mapeamento JPA

--- ecommerce-backend/src/main/java/com/domain/Producer.java
+++ ecommerce-backend/src/main/java/com/domain/Producer.java
@@ -1,5 +1,23 @@
 package com.domain;
 
-public class Producer {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_producer")
+public class Producer extends Person{
+
+	private String cnpj;
+	
+	public Producer() {
+		
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 }
