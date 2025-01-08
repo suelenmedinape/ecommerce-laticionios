afd3db1d0289312f260545151119873fd536f0a6
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-07 22:37:23 -0300

    feat: adicionar classe Client com mapeamento JPA

--- ecommerce-backend/src/main/java/com/domain/Client.java
+++ ecommerce-backend/src/main/java/com/domain/Client.java
@@ -1,5 +1,38 @@
 package com.domain;
 
-public class Client {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client extends Person{
+
+	private String cpf;
+	
+	@OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+	private List<Order> orders;
+
+	public Client() {
+		super();
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
 }
