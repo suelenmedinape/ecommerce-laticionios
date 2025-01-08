24f0ec0e27fc834f773b1a7e6cfa8b57d5a24e71
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-08 16:20:37 -0300

    feat: implementar serviço de cadastro de cliente com verificação de duplicidade

--- ecommerce-backend/src/main/java/com/services/ClientService.java
+++ ecommerce-backend/src/main/java/com/services/ClientService.java
@@ -3,13 +3,25 @@ package com.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.domain.Client;
+import com.exceptions.EmailAlreadyRegisteredException;
 import com.repositories.ClientRepository;
 
 @Service
 public class ClientService {
 
-	@Autowired
-	private ClientRepository clientRepository;
-	
-	
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client findByEmail(String email) {
+        return clientRepository.findByEmail(email);
+    }
+
+    public void register(Client client) {
+        if (clientRepository.findByEmail(client.getEmail()) != null) {
+            throw new EmailAlreadyRegisteredException("Email já registrado.");
+        }
+        clientRepository.save(client);
+    }
 }
+
