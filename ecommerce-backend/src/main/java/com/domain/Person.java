8c3ed6845c33ff1eb4080a6ec6fcf4ae57803b0e
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-07 22:34:30 -0300

    feat: adicionar classe Person com mapeamento JPA

--- ecommerce-backend/src/main/java/com/domain/Person.java
+++ ecommerce-backend/src/main/java/com/domain/Person.java
@@ -1,5 +1,75 @@
 package com.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+
+@MappedSuperclass
 public class Person {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address address;
+	
+	public Person() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 }
