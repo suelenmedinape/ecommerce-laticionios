569fece4206263813776af94b43fec68a1453f85
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-08 17:56:55 -0300

    feat: implementar controller de listagem de produtos

--- ecommerce-backend/src/main/java/com/controller/ProductController.java
+++ ecommerce-backend/src/main/java/com/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.domain.Product;
 import com.dtos.ProductDTO;
+import com.dtos.ProductSummaryDTO;
 import com.services.ProductService;
 
 import jakarta.validation.Valid;
@@ -35,5 +39,14 @@ public class ProductController {
 		
 		Map<String, String> response = Map.of("message","Cadastro realizado com sucesso");
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ProductSummaryDTO>> listProducts(){
+		List<Product> products = productService.listProducts();
+		List<ProductSummaryDTO> productSummaryDTO = products.stream()
+				.map(x -> new ProductSummaryDTO(x)).toList();
+		
+		return ResponseEntity.ok(productSummaryDTO);
 	}
 }
