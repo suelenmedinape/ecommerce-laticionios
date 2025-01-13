4969590d3bb8143c6a3970bc4969daa1e883107b
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-13 16:14:21 -0300

    feat: criar classe CartItem para representar itens do carrinho

---
+++ ecommerce-backend/src/main/java/com/domain/CartItem.java
@@ -0,0 +1,84 @@
+package com.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_cart_item")
+public class CartItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer quantity;
+    private Double unitPrice;
+    private Double totalPrice;
+    
+    @JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "cart_id")
+	private Cart cart;
+	
+	@ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    
+	public CartItem() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
