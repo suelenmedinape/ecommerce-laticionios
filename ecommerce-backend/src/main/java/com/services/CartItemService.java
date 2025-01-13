7a2e0c22ccfa03c261eb4be6cf4c3c230eb120ee
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-13 17:26:47 -0300

    fix: corrigir injeção de dependência configurada incorretamente

--- ecommerce-backend/src/main/java/com/services/CartItemService.java
+++ ecommerce-backend/src/main/java/com/services/CartItemService.java
@@ -3,12 +3,12 @@ package com.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.repositories.CartRepository;
+import com.repositories.CartItemRepository;
 
 @Service
 public class CartItemService {
 
 	@Autowired
-	private CartItemService cartItemService;
+	private CartItemRepository cartItemRepository;
 	
 }
