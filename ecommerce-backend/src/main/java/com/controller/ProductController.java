505fb5ec562d213b7928f8402df7cc9dca941fc8
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-14 20:22:39 -0300

    feat: permitir atualizar informações de produtos (Controller e Service)

--- ecommerce-backend/src/main/java/com/controller/ProductController.java
+++ ecommerce-backend/src/main/java/com/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,5 +65,12 @@ public class ProductController {
 				.map(x -> new ProductSummaryDTO(x)).toList();
 		
 		return ResponseEntity.ok(dtos);
+	}
+	
+	@PutMapping("/{productId}")
+	public ResponseEntity<Void> updateProduct(@PathVariable Long productId, @RequestBody ProductDTO dto){
+		productService.updateProduct(productId, dto);
+				
+		return ResponseEntity.ok().build();
 	}
 }
--- ecommerce-backend/src/main/java/com/services/ProductService.java
+++ ecommerce-backend/src/main/java/com/services/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.Product;
+import com.dtos.ProductDTO;
 import com.exceptions.ProductNotFoundException;
 import com.repositories.ProductRepository;
 
@@ -14,22 +15,42 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	public void insert(Product product) {
 		productRepository.save(product);
 	}
-	
-	public List<Product> listProducts(){
+
+	public List<Product> listProducts() {
 		return productRepository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
 		return productRepository.findById(id)
 				.orElseThrow(() -> new ProductNotFoundException("Produto não encontrado."));
 	}
 
 	public List<Product> listProductsByName(String name) {
-		
+
 		return productRepository.findByProductNameContainingIgnoreCase(name);
+	}
+
+	public void updateProduct(Long productId, ProductDTO dto) {
+		Product product = productRepository.findById(productId)
+				.orElseThrow(() -> new ProductNotFoundException("Produto não encontrado."));
+		
+		if (dto.getProductName() != null) {
+			product.setProductName(dto.getProductName());
+		}
+		
+		if(dto.getDescription() != null) {
+			product.setDescription(dto.getDescription());
+		}
+		if(dto.getPrice() != null) {
+			product.setPrice(dto.getPrice());
+		}
+		
+		product.setQuantity(dto.getQuantity());
+
+		productRepository.save(product);
 	}
 }
