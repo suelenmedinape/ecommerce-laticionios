a09e602be584e1c94f1b26e3ff3fdb3443809e20
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-14 18:44:57 -0300

    feat: adicionar tratamento para ClientNotFoundException no ControllerAdvice

--- ecommerce-backend/src/main/java/com/controllerAdvice/GlobalExceptionHandler.java
+++ ecommerce-backend/src/main/java/com/controllerAdvice/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.exceptions.ClientNotFoundException;
 import com.exceptions.EmailAlreadyRegisteredException;
 import com.exceptions.ProductNotFoundException;
 import com.responses.ValidationErrorResponse;
@@ -33,6 +34,12 @@ public class GlobalExceptionHandler {
     
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Map<String, String>> handlerProductNotFoundException(ProductNotFoundException ex){
+    	Map<String, String> response = Map.of("message", ex.getMessage());
+    	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+    
+    @ExceptionHandler(ClientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlerClientNotFoundException(ClientNotFoundException ex){
     	Map<String, String> response = Map.of("message", ex.getMessage());
     	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
