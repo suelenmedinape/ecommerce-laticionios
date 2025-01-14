0e2dd0fd3eeff4066e2eb4d69f6fe2133e7bb128
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-14 16:13:38 -0300

    refactor: remover imports não utilizados

--- ecommerce-backend/src/main/java/com/services/CartService.java
+++ ecommerce-backend/src/main/java/com/services/CartService.java
@@ -1,7 +1,6 @@
 package com.services;
 
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
