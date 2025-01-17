021412dc1edd4f17abec7637306ed2b5e6cb6ad3
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-16 22:43:41 -0300

    feat: criar execeção personalizada OrderNotFoundException

---
+++ ecommerce-backend/src/main/java/com/exceptions/OrderNotFoundException.java
@@ -0,0 +1,10 @@
+package com.exceptions;
+
+public class OrderNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public OrderNotFoundException(String message) {
+		super(message);
+	}
+}
