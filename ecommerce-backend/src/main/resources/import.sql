9c249b92c3cf4a9e6111da6bb31ca007b8eae668
Jhonny <jhonatacastrozeboby@gmail.com>
2025-01-25 16:30:18 -0300

    feat: criar arquivo import.sql para injetar um produtor padrão no sistema

---
+++ ecommerce-backend/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+-- Inserir produtor
+  INSERT INTO tb_producer (id, cnpj, email, name, password, phone, role) VALUES (1, '12345678901', 'adm@email.com', 'Cliente Um', '$2a$10$gNNzbIzg4ruJ4AtLc5ztHuaYz8J9e8ieSlFUcqE0lDR3YGaCSLGBe', '1111111111', 'ROLE_ADMIN');
