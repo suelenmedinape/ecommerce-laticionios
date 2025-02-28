2bb52df2192b4442bba4afb0ed89a374e1c8d099
Jhonny <jhonatacastrozeboby@gmail.com>
2025-02-28 16:13:40 -0300

    refactor: mudar a estratégia para 'LAZY'

--- ecommerce-backend/src/main/java/com/domain/Order.java
+++ ecommerce-backend/src/main/java/com/domain/Order.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,7 +39,7 @@ public class Order {
 	@ManyToOne
 	private Client client;
 	
-	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<OrderItem> orderItems = new ArrayList<>();
 	
 	public Order() {
