6f0f7957c90b02e0600d9f085155fc9788b462bd
Jhonny <jhonatacastrozeboby@gmail.com>
2025-03-09 09:48:27 -0300

    feat: adicionar busca pela categoria do produto

--- ecommerce-backend/src/main/java/com/repositories/ProductRepository.java
+++ ecommerce-backend/src/main/java/com/repositories/ProductRepository.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.domain.Product;
-import com.enums.Categories;
+import com.enums.Category;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 	List<Product> findByProductNameContainingIgnoreCase(String name);
 
-	List<Product> findByCategory(Categories category);
+	List<Product> findByCategory(Category category);
 }
