1f062bdc68768df118cd62ab4739b3acdb9940f8
Jhonny <jhonatacastrozeboby@gmail.com>
2025-03-11 16:15:40 -0300

    feat: adiciona consulta para buscar produtos com quantidade menor ou igual

--- ecommerce-backend/src/main/java/com/repositories/ProductRepository.java
+++ ecommerce-backend/src/main/java/com/repositories/ProductRepository.java
@@ -12,4 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
 	List<Product> findByProductNameContainingIgnoreCase(String name);
 
 	List<Product> findByCategory(Category category);
+	
+	List<Product> findByQuantityLessThanEqual(int quantity);
 }
